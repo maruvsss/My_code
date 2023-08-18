515211f68dceca483f99aae9f07097b4d1688bff
maruv.sss <gurniksasha2001@icloud.com>
2023-08-19 01:33:00 +0300

    adden hendler start and start text

--- python/gpt.py
+++ python/gpt.py
@@ -5,7 +5,12 @@ from telebot.async_telebot import AsyncTeleBot
 
 bot = AsyncTeleBot("6656005110:AAGMMMh1dCFYZcbpJE0mwZfYtZnaLMuM0b0")
 openai.organization = "org-jYPBmx4I02yL8Pvxf0EU56I7"
-openai.api_key = "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"
+openai.api_key = "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"
+
+
+@bot.message_handler(commands=['start'])
+async def start(message):
+    await bot.send_message(message.chat.id,'🎬 Привет! Я - твой помощник по поиску фильмов. Просто напиши мне краткое описание фильма, и я постараюсь найти его название для тебя. 😊\n\n🔍 Давай начнем! Просто отправь мне описание фильма, а я постараюсь найти его название.\n\nВот пример корректного запроса :\n<b>Пожалуйста, найди фильм, в котором главный герой путешествует во времени и пытается изменить прошлое, чтобы спасти свою семью</b>',parse_mode='html')
 
 @bot.message_handler()
 async def message(message):
