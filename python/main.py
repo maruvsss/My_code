2ff1d2af2cdff10892d788274dbd6781c56ee2e2
maruv.sss <gurniksasha2001@icloud.com>
2023-08-18 23:50:30 +0300

    create scan numbers

--- python/main.py
+++ python/main.py
@@ -1,19 +1,8 @@
 from tkinter import *
 from tkinter import ttk
-from speedtest import Speedtest
-import time
-
-elapsed_time = time.time()
-def test():
-    text_main.config(text="Результаты теста")
-    download = Speedtest().download()
-    upload = Speedtest().upload()
-    download_speed = round(download / (10 ** 6), 2)
-    upload_speed = round(upload / (10 ** 6), 2)
-
-    download_label.config(text="Скорость загрузки: " + str(download_speed) + "MbPs")
-    upload_label.config(text="Скорость отдачи: " + str(upload_speed) + "MbPs")
-
+from tkinter import messagebox
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
 
 root = Tk()
 # Name
@@ -22,17 +11,34 @@ root.title('maruv.sss')
 root.geometry("400x400")
 # Установка иконки
 root.iconbitmap(default="logo_2.ico")
-button = ttk.Button(root, text='Начать', command=test)
-text_main = Label(root, text='maruv.sss', font=("Arial", 14),foreground="#B71C1C")
-text_main.pack()
+x = phonenumbers.parse("+442083661177", None)
+print(x)
 
 
-download_label = Label(root, text='Скорость загрузки: ', font=30)
-download_label.pack(pady=(50, 0))
-upload_label = Label(root, text='Скорость отдачи: ', font=30)
-upload_label.pack(pady=10)
+def error():
+    message = messagebox.showinfo('⛔️ Error you fagges') * 10
+    return message
 
-button.pack(anchor='center', expand=True, ipadx=10, ipady=10)
+
+def on_button_click():
+    user_input = txt.get()
+    number_1 = phonenumbers.parse(user_input)
+    number_2 = geocoder.description_for_number(number_1, "ru")
+    number_3 = carrier.name_for_number(number_1, "ru")
+    result_label.config(text=f"{number_1}\n\n{number_2}\n\n{number_3}")
+
+
+label = Label(text="Для того что бы отсканировать номер\nвведите его в таком формате: +380668679044",foreground="#A52A2A",font=("Arial Bold", 12)) # создаем текстовую метку
+label.pack(pady=10)
+
+txt = Entry(root, width=40)
+txt.pack(anchor=S,pady=40)
+
+result_label = Label(root, text="", font=40)
+result_label.pack()
+
+btn = ttk.Button(text="Scan", command=on_button_click)
+btn.place(relx=0.5, rely=0.9, anchor=S, width=100, height=40)
 
 # Позволяет не закрываться программе пока, пользователь этого не захочет
 root.mainloop()
