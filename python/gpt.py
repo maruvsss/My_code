01ae75cda1ebd0603a3818be59df8518e8600503
maruv.sss <gurniksasha2001@icloud.com>
2023-08-19 02:40:25 +0300

    fix setting gpt

--- python/gpt.py
+++ python/gpt.py
@@ -3,29 +3,29 @@ import openai
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
 
-bot = AsyncTeleBot("6656005110:AAGMMMh1dCFYZcbpJE0mwZfYtZnaLMuM0b0")
+bot = AsyncTeleBot("6480636180:AAGsu2fusbzjRM1hBNH5UtGSVcWKNpc_Hoc")
 openai.organization = "org-jYPBmx4I02yL8Pvxf0EU56I7"
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 @bot.message_handler(commands=['start'])
 async def start(message):
+    print(message.from_user.id)
     await bot.send_message(message.chat.id,'🎬 Привет! Я - твой помощник по поиску фильмов. Просто напиши мне краткое описание фильма, и я постараюсь найти его название для тебя. 😊\n\n🔍 Давай начнем! Просто отправь мне описание фильма, а я постараюсь найти его название.\n\nВот пример корректного запроса :\n<b>Пожалуйста, найди фильм, в котором главный герой путешествует во времени и пытается изменить прошлое, чтобы спасти свою семью</b>',parse_mode='html')
 
 @bot.message_handler()
 async def message(message):
-    user_message = message.text  # Get the user's message
-    model_input = [{"role": "system", "content": "You are a helpful assistant."},
-                   {"role": "user", "content": user_message}]
+    user_message = message.text  # Получаем сообщение пользователя
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=model_input,
+        messages=[{"role": "system", "content": "Вы — помощник по поиску фильмов."},
+                  {"role": "user", "content": user_message}],
         temperature=0.5,
         max_tokens=256
     )
 
-    bot_response = response['choices'][0]['message']['content']
-    await bot.send_message(message.chat.id,bot_response)
+    bot_response = response.choices[0]['message']['content']
+    await bot.send_message(message.chat.id, bot_response)
 
 asyncio.run(bot.polling(non_stop=True))
