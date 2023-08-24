ce340320d0e3fb93a8436e42b544414bac6f424c
maruv.sss <gurniksasha2001@icloud.com>
2023-08-25 01:40:17 +0300

    adden database sqlite3 and command for admin sendall

--- TikTokSavee Bot/ttsavee_bot.py
+++ TikTokSavee Bot/ttsavee_bot.py
@@ -3,12 +3,42 @@ import sqlite3
 import asyncio
 import os
 import aiohttp
+import datetime
 from telebot.async_telebot import AsyncTeleBot
+from telebot import types
 from dotenv import load_dotenv, find_dotenv
+
 
 load_dotenv(find_dotenv())
 bot = AsyncTeleBot(os.getenv('TOKEN_BOT'))
 
+db = sqlite3.connect('db/ttsavee.db',check_same_thread=False)
+sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    tg_id integer,
+    date date
+)""")
+
+admin_id = 1900666417
+
+@bot.message_handler(commands=['sendall'])
+async def send_all_message(message: types.Message):
+
+    sql.execute("SELECT tg_id FROM users;")
+    users = sql.fetchall()
+    if message.chat.id == admin_id:
+        await bot.send_message(message.chat.id,'Starting')
+        for i in users:
+            try:
+                print("Send to: ", str(i[0]))
+                await bot.send_message(i[0],message.text[message.text.find(' '):])
+            except Exception as error:
+                print("Blocked bot: ", str(i[0]))
+            #await bot.send_message(i[0],message.text[message.text.find(' '):],parse_mode='html')
+    else:
+        await bot.send_message(message.chat.id,'Вы не являетесь администратором!')
 
 async def download(url):
     async with aiohttp.ClientSession() as session:
@@ -21,6 +51,15 @@ async def download(url):
 
 @bot.message_handler(commands=['start'])
 async def command_start(message):
+    date = datetime.datetime.now()
+    tg_id = message.from_user.id
+
+    sql.execute(f"SELECT tg_id FROM users WHERE tg_id={tg_id}")
+    data = sql.fetchone()
+    if data is None:
+        sql.execute("INSERT INTO users VALUES (?,?,?)",(None,tg_id,date))
+        db.commit()
+
     await bot.send_message(message.chat.id,
                            '<b>Привет! Добро пожаловать к нам в видеобот TikTok! 🎉</b>\n\nМы рады видеть тебя здесь. Просто дайте мне ссылку на видео с TikTok, и я отправлю вам это видео без водяных знаков отправителя. Наслаждайтесь просмотром! Если у тебя есть какие-либо вопросы или запросы, не стесняйся спрашивать. 😊📹',
                            parse_mode='html')
