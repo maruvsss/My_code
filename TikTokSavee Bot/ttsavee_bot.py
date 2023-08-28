d7a5bfe73c58a140be84ad1f28157e2700ea4662
maruv.sss <gurniksasha2001@icloud.com>
2023-08-29 02:38:39 +0300

    adden def sub channels

--- TikTokSavee Bot/ttsavee_bot.py
+++ TikTokSavee Bot/ttsavee_bot.py
@@ -4,17 +4,11 @@ import asyncio
 import os
 import aiohttp
 import datetime
-
-import pyshorteners
 import requests
+import telebot
 from telebot.async_telebot import AsyncTeleBot
 from telebot import types
 from dotenv import load_dotenv, find_dotenv
-
-
-import imageio
-imageio.plugins.ffmpeg.download()
-
 
 load_dotenv(find_dotenv())
 bot = AsyncTeleBot(os.getenv('TOKEN_BOT'))
@@ -30,6 +24,32 @@ sql.execute("""CREATE TABLE IF NOT EXISTS users(
 
 admin_id = 1900666417
 video = None
+CHANNELS = [["1й Канал ", "-1001717313870", "https://t.me/maruvsss"],
+            ["2й Канал ", "-1001717313870", "https://t.me/maruvsss"]]
+
+
+# keyboard = telebot.types.InlineKeyboardMarkup()
+# subscribe = telebot.types.InlineKeyboardButton(text="Подписаться", url="https://t.me/maruvsss")
+# check = telebot.types.InlineKeyboardButton(text="Проверить", callback_data="check")
+# keyboard.add(subscribe,check)
+
+keyboard = types.InlineKeyboardMarkup()
+
+for channel in CHANNELS:
+    btn = types.InlineKeyboardButton(text=channel[0], url=channel[2])
+    keyboard.add(btn)
+button_done_sub = types.InlineKeyboardButton(text='♻️ Проверить', callback_data='subchanneldone')
+keyboard.add(button_done_sub)
+
+
+
+async def check_sub_channels(channels, user_id):
+    for channel in channels:
+        chat_member = await bot.get_chat_member(chat_id=channel[1], user_id=user_id)
+        if chat_member.status == 'left':
+            return False
+    return True
+
 
 @bot.message_handler(commands=['sendall'])
 async def send_all_message(message: types.Message):
@@ -40,7 +60,7 @@ async def send_all_message(message: types.Message):
         for i in users:
             try:
                 print("Send to: ", str(i[0]))
-                await bot.send_message(i[0], message.text[message.text.find(' '):],parse_mode='html')
+                await bot.send_message(i[0], message.text[message.text.find(' '):], parse_mode='html')
             except Exception as error:
                 print("Blocked bot: ", str(i[0]))
             # await bot.send_message(i[0],message.text[message.text.find(' '):],parse_mode='html')
@@ -60,48 +80,64 @@ async def download(url):
 
 @bot.message_handler(commands=['start'])
 async def command_start(message):
-    date = datetime.datetime.now()
-    tg_id = message.from_user.id
+    if await check_sub_channels(CHANNELS, message.from_user.id):
+        date = datetime.datetime.now()
+        tg_id = message.from_user.id
 
-    sql.execute(f"SELECT tg_id FROM users WHERE tg_id={tg_id}")
-    data = sql.fetchone()
-    if data is None:
-        sql.execute("INSERT INTO users VALUES (?,?,?)", (None, tg_id, date))
-        db.commit()
+        sql.execute(f"SELECT tg_id FROM users WHERE tg_id={tg_id}")
+        data = sql.fetchone()
+        if data is None:
+            sql.execute("INSERT INTO users VALUES (?,?,?)", (None, tg_id, date))
+            db.commit()
 
-    await bot.send_message(message.chat.id,
-                           '<b>Привет! Добро пожаловать к нам в видеобот TikTok! 🎉</b>\n\nМы рады видеть тебя здесь. Просто дайте мне ссылку на видео с TikTok, и я отправлю вам это видео без водяных знаков отправителя. Наслаждайтесь просмотром! Если у тебя есть какие-либо вопросы или запросы, не стесняйся спрашивать. 😊📹',
-                           parse_mode='html')
+        await bot.send_message(message.chat.id,
+                               '<b>Привет! Добро пожаловать к нам в видеобот TikTok! 🎉</b>\n\nМы рады видеть тебя здесь. Просто дайте мне ссылку на видео с TikTok, и я отправлю вам это видео без водяных знаков отправителя. Наслаждайтесь просмотром! Если у тебя есть какие-либо вопросы или запросы, не стесняйся спрашивать. 😊📹',
+                               parse_mode='html')
+    else:
+        await bot.send_message(message.chat.id, '❗ Для доступа к боту,необходимо подписаться на канал',
+                               reply_markup=keyboard)
 
 
 @bot.message_handler()
 async def process(message):
     if re.compile('https://[a-zA-Z]+.tiktok.com/').match(message.text):
-        sticker = await bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEKIxtk6pqNbeyXirz3RDS4vp2oXIjzyQACeQAD5KDOB6RRas-jTv2HMAQ")
+        # sticker = await bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEKIxtk6pqNbeyXirz3RDS4vp2oXIjzyQACeQAD5KDOB6RRas-jTv2HMAQ")
         loading = await bot.send_message(message.chat.id, '🕗 Ожидайте видео скачивается...')
         video = await download(message.text)
 
         try:
 
-                await bot.delete_message(message.chat.id, loading.message_id)
-                await bot.delete_message(message.chat.id, sticker.message_id)
-                await bot.send_video(message.chat.id, video, caption='🎉 Поздравляю, видео успешно скачено!')
+            await bot.delete_message(message.chat.id, loading.message_id)
+            # await bot.delete_message(message.chat.id, sticker.message_id)
+            await bot.send_video(message.chat.id, video, caption='🎉 Поздравляю, видео успешно скачено!')
 
         except:
-            loading = await bot.send_message(message.chat.id, '😅 Ого ,тяжеленький\n🕗 Ожидайте видео скачивается...')
+            loading = await bot.send_message(message.chat.id, '😅 Ого ,тяжеленький, ожидайте видео скачивается...')
             response = requests.get(video)
             with open("ttsavee.mp4", "wb") as file:
                 file.write(response.content)
             result = open("ttsavee.mp4", 'rb')
 
-            await bot.send_document(message.chat.id, result,caption='🎉 Поздравляю, видео успешно скачено!')
+            await bot.send_document(message.chat.id, result, caption='🎉 Поздравляю, видео успешно скачено!')
             await bot.delete_message(message.chat.id, loading.message_id)
             os.remove("ttsavee.mp4")
 
     else:
         await bot.send_message(message.chat.id,
-                            '⛔️ В данный момент возможность загрузки видео доступна только из <b>TikTok</b>',
-                           parse_mode='html')
+                               '⛔️ В данный момент возможность загрузки видео доступна только из <b>TikTok</b>',
+                               parse_mode='html')
+@bot.callback_query_handler(func=lambda callback: callback.data)
+async def callback(callback):
+    if callback.data == "subchanneldone":
+        await bot.delete_message(callback.message.chat.id, callback.message.message_id)
+
+        if await check_sub_channels(CHANNELS, callback.message.from_user.id):
+            await bot.send_message(callback.message.chat.id,
+                                   '<b>Привет! Добро пожаловать к нам в видеобот TikTok! 🎉</b>\n\nМы рады видеть тебя здесь. Просто дайте мне ссылку на видео с TikTok, и я отправлю вам это видео без водяных знаков отправителя. Наслаждайтесь просмотром! Если у тебя есть какие-либо вопросы или запросы, не стесняйся спрашивать. 😊📹',
+                                   parse_mode='html')
+        else:
+            await bot.send_message(callback.message.chat.id, '❗ Для доступа к боту,необходимо подписаться на канал',
+                                   reply_markup=keyboard)
 
 
 asyncio.run(bot.polling(non_stop=True))
